419fc6f7232863dbfdde64a41092c631381827ec
Dawid Sudwoj <allmemorytime@gmail.com>
2020-10-25 15:56:35 +0100

    code shortening and add descryption and style

--- js/script.js
+++ js/script.js
@@ -8,54 +8,35 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-}
-
-else if(playerInput == '2'){
-    playerMove = 'papier';
-  }
-
-else if(playerInput == '3'){
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
     playerMove = 'nożyce';
 }  
-
 printMessage('Twój ruch to: ' + playerMove);
-
 // teraz komputera ruch losowy
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 console.log('Wylosowana liczba komputera to: ' + randomNumber);
-
 let computerMove = 'nieznany ruch';
-
 if(randomNumber == '1'){
-    computerMove = 'kamień';
-  }
-
-else if(randomNumber == '2'){
-    computerMove = 'papier';
-    }
-
-else if(randomNumber == '3'){
-    computerMove = 'nożyce';
-  }
-
+  computerMove = 'kamień';
+} else if(randomNumber == '2'){
+  computerMove = 'papier';
+} else if(randomNumber == '3'){
+  computerMove = 'nożyce';
+}
 printMessage('Komputer: ' + computerMove);
-
 //wygrane
 
 if( playerMove == 'kamień' && computerMove == 'nożyce'){
     printMessage('Wynik meczu: Wygrywasz!');
-}
-
-else if( playerMove == 'papier' && computerMove == 'kamień'){
+} else if( playerMove == 'papier' && computerMove == 'kamień'){
     printMessage('Wynik meczu: Wygrywasz!');
 }
 
 else if( playerMove == 'nożyce' && computerMove == 'papier'){
-    printMessage('Wynik meczu: Wygrywasz!');
+  printMessage('Wynik meczu: Wygrywasz!');
 }
-
 //remis
 
 else if( playerMove == computerMove ){
@@ -70,13 +51,7 @@ else {
   printMessage('Przegrałeś!')
 }
 
-function buttonClicked(){
-  printMessage('Guzik został kliknięty');
-}
 
-let testButton = document.getElementById('test-button');
-
-testButton.addEventListener('click', buttonClicked);
 
 
 
